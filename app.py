b89a63647ef6e9a2892d3c3cb64370140cc09dfd
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-07-10 10:34:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class InferlessPythonModel:
     def infer(self, inputs):
         pipeline_output = self.generator(
             inputs['prompt'],
-            max_length=150,
+            max_length=100,
             do_sample=True,
             top_k=10,
             num_return_sequences=1,
